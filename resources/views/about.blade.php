da812f43e4ed318058fc4a88a4e33ac8d6a66058
jonathanmendoza976 <jonathan.mendoza1708@gmail.com>
2022-11-11 01:23:20 -0600

    routes added

---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout.layout>
+
+<h1>Conoce</h1>
+
+</x-layout.layout>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,14 @@ Route::get('/welcome', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
 Route::get('/contact', function () {
     return view('contact');
 });
